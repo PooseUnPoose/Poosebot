1c3fd15e23b8d645a14adc23b5372f7dbce2f89d
Michael.Whitehead <michael.whitehead@student.ufv.ca>
2025-05-22 23:11:11 -0700

    requirements file

--- Poosebot.py
+++ Poosebot.py
@@ -1,8 +1,6 @@
 import asyncio
 import discord
 from discord.ext import commands
-import random
-import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
